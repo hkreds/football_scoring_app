c85197a6df1e9a6be4a7a0f804c010d50414fbad
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-03 01:34:57 +0000

    [Add]ルートパス設定とviewファイル、会員用のviewファイル

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,17 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
   
+  scope module: :public do
+    root to: "homes#top"
+  end
+  
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  namespace :admin do
+    
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
