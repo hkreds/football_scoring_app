01e876cc1d11ce1b5a106a63766ac4b6c12d6df7
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-03 05:59:15 +0000

    [Add]管理者用大会登録・編集機能

--- app/controllers/admin/conventions_controller.rb
+++ app/controllers/admin/conventions_controller.rb
@@ -1,2 +1,39 @@
 class Admin::ConventionsController < ApplicationController
+  def index
+    @convention = Convention.new
+    @conventions = Convention.page(params[:page])
+  end
+  
+  def create
+    @convention = Convention.new(convention_params)
+    if @convention.save
+      redirect_to request.referer
+    else
+      @conventions = Genre.all
+      render :index
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+    @convention = Convention.find(params[:id])
+  end
+
+  def update
+    @convention = Convention.find(params[:id])
+    if @convention.update(convention_params)
+      redirect_to admin_conventions_path
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def convention_params
+    params.require(:convention).permit(:name,:start_date,:end_date)
+  end
+  
 end
