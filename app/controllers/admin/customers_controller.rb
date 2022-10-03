e6ca029e2e2f2a7b92f1fc05e1c01f494b0b2cde
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-03 05:11:53 +0000

    [Add]管理者側会員編集機能

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,22 @@
 class Admin::CustomersController < ApplicationController
+  def show
+    @customer = Customer.find(params[:id])
+  end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :introduction, :profile_image, :email, :is_deleted)
+  end
+  
 end
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
+    @customers = Customer.page(params[:page]).per(20)
   end
 end
