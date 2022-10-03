ae598760d5d72ef43358d0910541464c8126a409
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-03 07:42:02 +0000

    [Add]管理者用選手登録・編集機能

--- app/controllers/admin/players_controller.rb
+++ app/controllers/admin/players_controller.rb
@@ -1,2 +1,40 @@
 class Admin::PlayersController < ApplicationController
+  def index
+    @player = Player.new
+    @players = Player.page(params[:page])
+  end
+  
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to request.referer
+    else
+      @players = Player.all
+      render :index
+    end
+  end
+  
+  def show
+    @player = Player.find(params[:id])
+  end
+  
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+    if @player.update(player_params)
+      redirect_to admin_players_path
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def player_params
+    params.require(:player).permit(:name,:birthday,:height,:weight)
+  end
+  
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,7 @@
 class Player < ApplicationRecord
+  has_many :player_scorings
+  has_many :affiliated_players
+  has_many :competitors
+  
+  validates :name, presence: true
 end
