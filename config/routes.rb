22324762e867b3ff8ce9748d76015b17d099d47c
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-03 02:24:50 +0000

    [Add]管理者側ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   scope module: :public do
     root to: "homes#top"
     patch 'customers/:id/withdraw' => 'customers#withdraw', as: 'withdraw'
@@ -20,14 +20,20 @@ Rails.application.routes.draw do
     resources :scorings, only: [:index]
     resources :teams, only: [:index,:show]
   end
-  
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
   namespace :admin do
-    
+    root to: "homes#top"
+    resources :customers, only: [:show,:edit,:update]
+    resources :players, only: [:index,:create,:show,:edit,:update]
+    resources :teams, only: [:index,:create,:show,:edit,:update]
+    resources :coventions, only: [:index,:create,:show,:edit,:update]
+    resources :matches, only: [:index,:create,:show,:edit,:update]
+    resources :scorings, only: [:index,:show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
