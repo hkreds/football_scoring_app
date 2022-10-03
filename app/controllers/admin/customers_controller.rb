24ca0d2246287a2567e6ea78b8230386eafdda8d
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-03 06:30:10 +0000

    [Add]会員モデルのバリデーション、管理者用会員編集ページのエラーメッセージ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,15 +2,18 @@ class Admin::CustomersController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
   end
-  
+
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
-    @customer.update(customer_params)
-    redirect_to admin_customer_path(@customer)
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
   end
 
   private
@@ -18,5 +21,5 @@ class Admin::CustomersController < ApplicationController
   def customer_params
     params.require(:customer).permit(:name, :introduction, :profile_image, :email, :is_deleted)
   end
-  
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,9 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  
+  validates :name, presence: true
+  
   has_one_attached :profile_image
   
   def get_profile_image(width, height)
