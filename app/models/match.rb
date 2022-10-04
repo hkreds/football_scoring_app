01f5a8bbb6bf05bbea3735e6718035841f1fb794
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-04 05:27:04 +0000

    [Fix]試合モデル、試合のチーム名表示

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,8 @@
 class Match < ApplicationRecord
   belongs_to :convention
-  belongs_to :team, optional: true
+  #試合でチームはホームチームとアウェイチームに分かれます。
+  belongs_to :home_team, class_name: "Team", optional: true
+  belongs_to :away_team, class_name: "Team", optional: true
   has_many :competitors, dependent: :destroy
   has_many :scorings, dependent: :destroy
   
