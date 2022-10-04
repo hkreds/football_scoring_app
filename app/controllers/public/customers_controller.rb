39a68b5b44622cbe231e574bce447ab334f44476
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-04 06:55:54 +0000

    [Add]会員側の会員編集機能と退会ボタンの作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,37 @@
 class Public::CustomersController < ApplicationController
+  def index
+    @customers = Customer.page(params[:page]).per(10)
+  end
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer.id)
+    else
+      render :edit
+    end
+  end
+  
+  def withdraw
+    @customer = Customer.find(params[:id])
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:name, :introduction, :profile_image, :email)
+  end
+  
 end
