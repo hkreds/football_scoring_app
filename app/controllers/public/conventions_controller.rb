b1894c710e6d27abb2b4c1b9da2ba27b7302f640
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-06 07:45:15 +0000

    [Add]会員側の大会詳細ページ

--- app/controllers/public/conventions_controller.rb
+++ app/controllers/public/conventions_controller.rb
@@ -2,4 +2,11 @@ class Public::ConventionsController < ApplicationController
   def index
     @conventions = Convention.page(params[:page])
   end
+  
+  def show
+    @convention = Convention.find(params[:id])
+    @participating_team = ParticipatingTeam.new
+    @participating_teams = @convention.participating_teams.all
+    @matches = Match.where(convention_id: @convention.id)
+  end
 end
