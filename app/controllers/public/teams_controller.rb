39ab71167399eeef265610fbdce3a6fae73f6b71
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-06 08:35:42 +0000

    [Add]会員側チーム詳細

--- app/controllers/public/teams_controller.rb
+++ app/controllers/public/teams_controller.rb
@@ -2,4 +2,11 @@ class Public::TeamsController < ApplicationController
   def index
     @teams = Team.page(params[:page])
   end
+  
+  def show
+    @team = Team.find(params[:id])
+    @affiliated_player = AffiliatedPlayer.new
+    @affiliated_players = @team.affiliated_players.all
+  end
+  
 end
