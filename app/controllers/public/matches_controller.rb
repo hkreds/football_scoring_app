3c8bb84fc9e59f8f6c938113d2fedaac873701b1
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-06 08:14:19 +0000

    [Add]会員側試合詳細ページ

--- app/controllers/public/matches_controller.rb
+++ app/controllers/public/matches_controller.rb
@@ -1,2 +1,7 @@
 class Public::MatchesController < ApplicationController
+  def show
+    @match = Match.find(params[:id])
+    @competitor = Competitor.new
+    @competitors = @match.competitors.all
+  end
 end
