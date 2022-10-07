42038c22fd3e1cf92d6571432f802fe620ef0c5d
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-07 06:14:06 +0000

    [Fix]選手採点関係のアソシエーション

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -2,11 +2,12 @@ class Competitor < ApplicationRecord
   belongs_to :player
   belongs_to :match
   #複数の出場選手に採点を行いますが、出場選手は複数の採点をされます。
+  has_many :player_scorings
   has_many :scorings, through: :player_scorings
   accepts_nested_attributes_for :player_scorings, allow_destroy: true
-  
+
   enum position: { GK: 0, DF: 1, MF: 2, FW: 3 }
-  
+
   validates :uniform_number, presence: true
   validates :player_id, uniqueness:{
     scope: [:match_id]
--- app/models/scoring.rb
+++ app/models/scoring.rb
@@ -2,6 +2,7 @@ class Scoring < ApplicationRecord
   belongs_to :customer
   belongs_to :match
   #複数の出場選手に採点を行いますが、出場選手は複数の採点をされます。
+  has_many :player_scorings
   has_many :competitors, through: :player_scorings
   accepts_nested_attributes_for :player_scorings, allow_destroy: true
 end
