dbc52c3977e91d2f6697a7d977c9f9e59b7c6764
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-16 03:55:02 +0000

    [Fix]選手モデルのアソシエーション、会員側の試合並び順、チーム詳細画面

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,4 @@
 class Player < ApplicationRecord
-  has_many :player_scorings, dependent: :destroy
-  has_many :competitors, dependent: :destroy
   #チームは複数の選手が所属しますが、選手は複数のチームに所属する可能性があります。
   has_many :affiliated_players
   has_many :teams, through: :affiliated_players
