678de0e2c4f363efd5b1f7e97969196590ae944b
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-21 06:38:02 +0000

    [Delete]管理者側の選手削除機能

--- app/controllers/admin/players_controller.rb
+++ app/controllers/admin/players_controller.rb
@@ -33,12 +33,6 @@ class Admin::PlayersController < ApplicationController
     end
   end
   
-  def destroy
-    @player = Player.find(params[:id])
-    @player.destroy
-    redirect_to admin_players_path
-  end
-  
   private
 
   def player_params
