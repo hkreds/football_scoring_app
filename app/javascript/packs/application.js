c1808f09c24d4843f770b1fd4b260d4bb66d9616
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-22 02:02:10 +0000

    [Add]トップに戻るボタン

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,8 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
+// 追加のJS
+import "./add"
 
 Rails.start()
 Turbolinks.start()
