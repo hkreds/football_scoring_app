dde06d27352555f6337b63376a30525c13b7f662
hkreds <109258421+hkreds@users.noreply.github.com>
2022-11-11 09:36:14 +0000

    [Fix]会員側試合詳細コントローラーにpreload記述

--- app/controllers/public/matches_controller.rb
+++ app/controllers/public/matches_controller.rb
@@ -3,16 +3,16 @@ class Public::MatchesController < ApplicationController
   def show
     @match = Match.find(params[:id])
     @competitor = Competitor.new
-    @competitors = @match.competitors.all
+    @competitors = Competitor.preload(:player).where(match_id: @match.id)
     if params[:newer]
-      @scorings = @match.scorings.newer.page(params[:page]).per(10)
+      @scorings = Scoring.preload(:customer,:match).where(match_id: @match.id).newer.page(params[:page]).per(10)
     elsif params[:older]
-      @scorings = @match.scorings.older.page(params[:page]).per(10)
+      @scorings = Scoring.preload(:customer,:match).where(match_id: @match.id).older.page(params[:page]).per(10)
     elsif params[:favorites_count]
-      @scorings_favorites = @match.scorings.favorites_count
+      @scorings_favorites = Scoring.preload(:customer,:match).where(match_id: @match.id).favorites_count
       @scorings = Kaminari.paginate_array(@scorings_favorites).page(params[:page]).per(10)
     else
-      @scorings = @match.scorings.newer.page(params[:page]).per(10)
+      @scorings = Scoring.preload(:customer,:match).where(match_id: @match.id).newer.page(params[:page]).per(10)
     end
   end
 end
